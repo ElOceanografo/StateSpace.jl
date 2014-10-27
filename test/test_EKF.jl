e0f6999b80a7a6d19f83ae7b95f652bb62d5f193
Sam Urmy <eloceanografo@gmail.com>
2014-10-27 18:05:27 -0400

    change EKF test to Ricker model...more realistic

--- test/test_EKF.jl
+++ test/test_EKF.jl
@@ -2,18 +2,28 @@ using StateSpace
 
 println("Testing ExtendedKalmanFilter.jl...")
 
+# Define process model: Ricker growth model, with two populations
+r1 = 0.03 							# 10% intrinsic growth rate for pop 1
+r2 = 0.06 							# 14% intrinsic growth rate for pop 1
+K1 = 20000 							# carrying capacity for pop 1
+K2 = 10000 							# carrying capacity for pop 2
 
-f(x) = [0.2 * x[1]^2, 0.1 * x[2]^2]
-m = 2
-V = diagm([0.3, 0.1])
+# process model
+function f(x)
+	[x[1] .* exp(r1 * (1 - (x[1] ./ K1))), 
+	 x[2] .* exp(r2 * (1 - (x[2] ./ K2)))]
+end
 
-g(x) = [x[1], x[2]^2, x[1] * x[2]]
-n = 3
-W = diagm(ones(3) * 0.1)
+m = 2 								# dimension of latent state
+V = diagm([3000.0, 5000.0]) 		# covariance of (additive) process noise
 
+# Define observation model
+g(x) = 0.01 .* x
+n = 2
+W = diagm([100.0, 250.0])
 
 mod = NonlinearGaussianSSM(f, m, V, g, n, W)
-x0 = MvNormal([1.0, 1.0], [100.0 0.0; 0.0 100.0])
+x0 = MvNormal([1000.0, 500.0], [100.0 0.0; 0.0 100.0])
 
 x1 = predict(mod, x0)
 # println(mean(x1))
@@ -22,18 +32,28 @@ y1 = observe(mod, x1)
 u1 = update(mod, x1, mean(y1))
 # println(mean(u1))
 
-xx, yy = simulate(mod, 100, x0)
+xx, yy = simulate(mod, 200, x0)
 yy[1, 50] = NaN  
 
 # using PyPlot
-# plot(xx', "k")
-# plot(yy', "r")
+# subplot(211)
+# plot(xx')
+# title("True state")
+# subplot(212)
+# plot(yy')
+# title("Observations")
 # readline()
 
 fs = filter(yy, mod, x0)
 
-# plot(xx', "k")
-# plot(mean(fs)', "r")
+# figure()
+# for i in 1:2
+# 	subplot(2, 1, i)
+# 	plot(xx[i, :]')
+# 	plot(mean(fs)[i, :]', "k")
+# 	plot(mean(fs)[i, :]' + 1.96 * sqrt(var(fs))[i, :]', "gray")
+# 	plot(mean(fs)[i, :]' - 1.96 * sqrt(var(fs))[i, :]', "gray")
+# end
 # readline()
 
 y_new = fs.observations[:, end] + randn(mod.n) / 10
