6e395291791b994016e1e5f52fff509a7ac82132
Sam Urmy <samuel.urmy@stonybrook.edu>
2015-09-25 12:07:30 -0400

    Get rid of .m and .n fields of NonlinearGaussianSSM

--- src/ExtendedKalmanFilter.jl
+++ src/ExtendedKalmanFilter.jl
@@ -2,20 +2,18 @@ using ForwardDiff
 
 type NonlinearGaussianSSM{T} <: AbstractGaussianSSM
 	f::Function # actual process function
-	m::Int64
 	fjac::Function # function returning Jacobian of process
 	V::Matrix{T}
 	g::Function # actual observation function
-	n::Int64
 	gjac::Function # function returning Jacobian of process
 	W::Matrix{T}
 end
 
-function NonlinearGaussianSSM{T}(f::Function, m::Int64, V::Matrix{T}, 
-		g::Function, n::Int64, W::Matrix{T})
+function NonlinearGaussianSSM{T}(f::Function, V::Matrix{T}, 
+		g::Function, W::Matrix{T})
 	fjac = forwarddiff_jacobian(f, T, fadtype=:typed)
 	gjac = forwarddiff_jacobian(g, T, fadtype=:typed)
-	return NonlinearGaussianSSM{T}(f, m, fjac, V, g, n, gjac, W)
+	return NonlinearGaussianSSM{T}(f, fjac, V, g, gjac, W)
 end
 
 
