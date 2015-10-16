d936fef5caa969d41536dcaa79e81e81f801d591
JonnyCBB <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2015-10-17 00:37:57 +0100

    add example code showing how to use Unscented RTS Smoother

--- examples/UnscentedKalmanFilter_DampedOscillatorExample.jl
+++ examples/UnscentedKalmanFilter_DampedOscillatorExample.jl
@@ -37,7 +37,7 @@ processCovariance = 1e-2*[0.1 0;
     0 1e-3]
 
 #Set the observation noise covariance
-observationCovariance = 1e-1*eye(2)
+observationCovariance = 3e-2*eye(2)
 
 #Create additive noise UKF model
 ukfStateModel = AdditiveNonLinUKFSSM(processFunction, processCovariance,
@@ -54,7 +54,7 @@ trueInitialState = [1.4, 0.0]
 
 #Set the number of observations and generate the true values and the noisy
 #observations.
-numObs = 200
+numObs = 100
 trueState = zeros(2,numObs)
 noisyObs = zeros(2,numObs)
 trueState[:,1] = trueInitialState
@@ -116,6 +116,53 @@ oscillatorx1_state_plot = plot(
     Guide.xlabel("Time (seconds)"), Guide.ylabel("x1"),
     Guide.manual_color_key("Colour Key",["Filtered Estimate", "Measurements","True Value "],[getColors[1],getColors[2],getColors[3]]),
     Guide.title("Unscented Kalman Filter (Additive) Example")
+    )
+display(oscillatorx1_state_plot)
+#End Section: Plot results
+################################################################################
+
+
+################################################################################
+#Section: Perform Unscented Kalman Smoother
+#-------------------------------------------------------------------------------
+smoothed_state = smooth(ukfStateModel, filtered_state)
+#End Section: Perform Unscented Kalman Smoother
+################################################################################
+
+################################################################################
+#Section: Plot results
+#-------------------------------------------------------------------------------
+x_data = 1:numObs
+x1_array = Vector{Float64}(numObs)
+x1Var_array = Vector{Float64}(numObs)
+x1_Guess = initial_guess.μ[1]
+x1Var_Guess = 2*sqrt(initial_guess.Σ.mat[1,1])
+for i in x_data
+    current_state = smoothed_state.state[i]
+    x1_array[i] = current_state.μ[1]
+    x1Var_array[i] = 2*sqrt(current_state.Σ.mat[1,1])
+end
+df_fs = DataFrame(
+    x = x_data*Δt,
+    y = x1_array,
+    ymin = x1_array - x1Var_array,
+    ymax = x1_array + x1Var_array,
+    f = "Filtered values"
+    )
+
+n = 3
+getColors = distinguishable_colors(n, Color[LCHab(70, 60, 240)],
+                                   transform=c -> deuteranopic(c, 0.5),
+                                   lchoices=Float64[65, 70, 75, 80],
+                                   cchoices=Float64[0, 50, 60, 70],
+                                   hchoices=linspace(0, 330, 24))
+oscillatorx1_state_plot = plot(
+    layer(x=x_data*Δt, y=noisyObs[1,:], Geom.point, Theme(default_color=getColors[2])),
+    layer(x=x_data*Δt, y=trueState[1,:], Geom.line, Theme(default_color=getColors[3])),
+    layer(df_fs, x=:x, y=:y, ymin=:ymin, ymax=:ymax, Geom.line, Geom.ribbon),
+    Guide.xlabel("Time (seconds)"), Guide.ylabel("x1"),
+    Guide.manual_color_key("Colour Key",["Filtered Estimate", "Measurements","True Value "],[getColors[1],getColors[2],getColors[3]]),
+    Guide.title("Unscented Kalman Smoother (Additive) Example")
     )
 display(oscillatorx1_state_plot)
 #End Section: Plot results
